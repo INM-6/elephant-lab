4217bb14915c4976c1768dcff2422300007f95eb
Björn Müller <bj.mueller@fz-juelich.de>
2018-11-07 14:32:25 +0100

    Added Python Code execution and comms API usage
    Python Code is inside the .ts file because I'm unable to load additional
    files at the moment.
    Using Comms API to transmit data from kernel environment to TypeScript
    frontend. This can send raw data!

--- src/index.ts
+++ src/index.ts
@@ -36,10 +36,11 @@ const extension: JupyterLabPlugin<void> = {
 		var_place.setAttribute("id", "neo_ele_vars");
 		widget.node.appendChild(var_place);
 
-
 		function ioCallback(msg: KernelMessage.IIOPubMessage): void {
+			console.log("Got return from Kernel");
+			console.log(msg);
 			if(msg.header.msg_type == 'stream' && msg.content.name == 'stdout'){
-				console.log("Stdout: ", msg.content.text);
+				console.log("Stdout: ", msg.content);
 				let text = document.createTextNode(msg.content.text as string);
 				let old_text = var_place.childNodes[0];
 				if(old_text != null){
@@ -55,15 +56,76 @@ const extension: JupyterLabPlugin<void> = {
 			console.log("Session.ready: ", session.ready);
 
 			session.ready.then(() => {session.kernel.ready.then(() => {
+				//let array = Object.getOwnPropertyNames(session);
+				//console.log(array);
 
-				console.log("Kernel: ", session.kernel);
+				session.kernel.registerCommTarget('test2', (comm:any, commMsg:any):any => {
+					if(commMsg.content.target_name !== 'test2'){
+						return;
+					}
+					comm.onMsg = (msg:any) => {
+						console.log("MEEESSSAAAGGEEE ", msg.buffers[0]);
+						console.log(new Float64Array(msg.buffers[0].buffer));
+					};
+					comm.onClose = (msg:any) => {};
+				});
 
+				// Initialize function that returns variable list
+				let code: string;
+				fetch('packages/python/test.py').then(response => {
+					console.log("Reading code: ", response)
+					return response.json();
+				}).then(data => {
+					code = data;
+				});
+
+				code = 
+`import json
+from IPython.core.magics.namespace import NamespaceMagics
+from IPython import get_ipython
+#from neo.core import BaseNeo
+nsm = NamespaceMagics()
+nsm.shell = get_ipython().kernel.shell
+
+def testfunc():
+    nsm = NamespaceMagics()
+    nsm.shell = get_ipython().kernel.shell
+    from neo.core.baseneo import BaseNeo
+    from neo.core.block import Block
+    vals = nsm.who_ls()
+    values = [v for v in vals if isinstance(eval(v), (BaseNeo, list))] # in ['Block', 'Segment', 'ChannelIndex', 
+    for value in list(values):
+        val = eval(value)
+        if isinstance(val, list):
+            if len(val) > 0 and isinstance(val[0], BaseNeo):
+                    pass
+            else:
+                values.remove(value)
+        if isinstance(val, Block):
+            for i, seg in enumerate(val.list_children_by_class("Segment")):
+                #globals()[''.join(['blchidx', str(i)])] = chidx
+                values.append(''.join([value, '.segmets[', str(i), ']']))
+                for j, sig in enumerate(seg.analogsignals):
+                    #pass
+                    values.append(''.join([values[-1-j], '.analogsigs[', str(j), ']']))
+
+    return json.dumps(values)`
+				
+				console.log('Python Code: ', code);
+				let request_init: KernelMessage.IExecuteRequest = {
+					code: code,
+					stop_on_error: false,
+					store_history: false,
+				};
+				session.kernel.requestExecute(request_init);
+
+				// React to codecell execution, update variable list
 				NotebookActions.executed.connect(() => {
 
 					console.log("Cell executed"); 
 
 					let request: KernelMessage.IExecuteRequest = {
-						code: "print('This was printed by the Python Kernel')",
+						code: "print(testfunc())",
 						stop_on_error: false,
 						store_history: false,
 					};
