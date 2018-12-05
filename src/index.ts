7539646080c2942b277b17028315a33e7aa20514
Björn Müller <bj.mueller@fz-juelich.de>
2018-12-05 10:37:25 +0100

    Removed code for d3 and ipywidgets

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import {
 import {
 	KernelMessage, Kernel
 } from '@jupyterlab/services';
+<<<<<<< HEAD
 import * as CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/python/python';
@@ -20,99 +21,13 @@ import 'codemirror/mode/python/python';
 import {
     WidgetManager
 } from './manager';
+
 import {
 	IJupyterWidgetRegistry
 } from '@jupyter-widgets/base'; 
 //import * as d3 from 'd3';
 
 import '../style/index.css';
-
-/*
-
-   		var load_css = function() {
-			//$('#site').css({width: '70%'});
-			console.log("HEEEERE");
-        		var link = document.createElement("link");
-        		link.type = "text/css";
-        		link.rel = "stylesheet";
-        		link.href = require.toUrl("/nbextensions/new_ext/new_main.css");
-        		document.getElementsByTagName("head")[0].appendChild(link);
-			console.log("HEREEEEEE");
-			console.log(link);
-			$("body").append('<div><div id="content" style="display:inline-block;width=30%;"><p>Text</p></div></div>');
-			$.get("/nbextensions/new_ext/new_main.html", function (data) {$("#content").append(data);console.log("A "+ data);});
-			//$('#content').load("./../nbextensions/new_ext/new_main.html", function( response, status, xhr ) {console.log(xhr.statusText + " " +response);});
-    		};
-
-		var showd3 = function(d3){
-			console.log("D3 "+d3.version);
-			var data = [150, 230, 180, 90];
-
-			var svg = d3.select("#content")
-            		.append("svg")
-            		.attr("width", 300)
-            		.attr("height", 200);
- 			svg.selectAll(".bar")
-				.data(data)
-  				.enter()
-  				.append("rect")
-  				.attr({
-    					class : "bar",
-    					width : function(d) {return d;},
-    					height: "40",
-    					y : function(d, i) {return i*50 + 10;},
-    					x : "10"
-   				});
-			console.log("D3 finished");
-		};
-		var updateplot = function(d3){
-			//require.config({
-			 // paths: {
-			 //     d3: '//cdnjs.cloudflare.com/ajax/libs/d3/3.4.8/d3.min'
-			 // }
-			//});
-			//require(["d3"], function(d3) {console.log("D3 loaded: "+d3.version);});
-			console.log("D3 "+d3.version);
-			var data = [10, 100, 190];
-
-			var svg = d3.select("#content");
- 			var bars = svg.selectAll(".bar")
-				.data(data);
-			//bars.attr("class", "update");
-			// Remove unnecessary bars if data became less
-  			bars.exit().remove();
-			// Add new bars that might be needed additionally
-			bars.enter()
-  				.append("rect")
-  				.attr({
-    					class : "bar",
-    					width : function(d) {return d;},
-    					height: "40",
-    					y : function(d, i) {return i*50 + 10;},
-    					x : "10"
-   				});
-				//.merge(bars)
-				//	.data(function(d){return d;});
-			bars.transition()
-				.duration(0)
-				// Update all attributes
-  				.attr({
-    					class : "bar",
-    					width : function(d) {return d;},
-    					height: "40",
-    					y : function(d, i) {return i*50 + 10;},
-    					x : "10"
-   				});
-			console.log("D3 finished");
-		};
-
-		/*var updateplot = function(d3){
-			var data = [10, 100, 10, 100];
-			var svg = d3.select("#content").transition();
-			svg.selectAll(".bar")
-				.data(data)
-				.enter();
-		};*/
 
 
 /**
@@ -133,16 +48,6 @@ const extension: JupyterLabPlugin<void> = {
 		let var_place = document.createElement('div');
 		var_place.setAttribute("id", "neo_ele_vars");
 		widget.node.appendChild(var_place);
-
-		/*		// Setup for ipywidgets
-		let jupyter_area = document.createElement('div');
-		let input_area = document.createElement('div');
-		let widget_area = document.createElement('div');
-		input_area.setAttribute("class", "inputarea");
-		widget_area.setAttribute("class", "widgetarea");
-		jupyter_area.appendChild(input_area);
-		jupyter_area.appendChild(widget_area);*/
-
 
 
 		function ioCallback(msg: KernelMessage.IIOPubMessage): void {
@@ -171,121 +76,6 @@ const extension: JupyterLabPlugin<void> = {
 			console.log("Session.ready: ", session.ready);
 
 			session.ready.then(() => {session.kernel.ready.then(() => {
-				//let array = Object.getOwnPropertyNames(session);
-				//console.log(array);
-				//
-				//////////////////////////////////////////
-				// IPyWidgets
-				var HelloView = widgets.DOMWidgetModel.extend({
-    		
-					render: function() {
-
-						let var_place = document.createElement('div');
-						var_place.setAttribute("id", "neo_ele_vars_333");
-						let var_place2 = document.createElement('p');
-						var_place2.setAttribute("id", "neo_ele_vars222");
-						var_place.appendChild(var_place2);
-						let text = document.createTextNode("AAAAAAAAAAAAAAAA");
-						var_place2.appendChild(text);
-
-						this.el = var_place;
-						widget.node.appendChild(var_place);
-						console.log("HEEEERERERERE");
-						//.textContent = "AAAAAAABBBBBBBCCCCCCDDDDD";
-        }
-			});
-				var HelloShow = HelloView();
-				widgets.registerWidget(HelloShow);
-				HelloShow.render();
-				widget.node.appendChild(HelloShow);
-				console.log(widget.node);
-				console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
-				////////////////////////////////////////////
-
-				// Setup for ipywidgets
-				let jupyter_area = document.createElement('div');
-				let widget_text = document.createTextNode("Hierunter sollte das Widget stehen");
-				jupyter_area.appendChild(widget_text);
-        	        	let input_area = document.createElement('div');
-                		let widget_area = document.createElement('div');
-             	   		input_area.setAttribute("class", "inputarea");
-                		widget_area.setAttribute("class", "widgetarea");
-                		jupyter_area.appendChild(input_area);
-				jupyter_area.appendChild(widget_area);
-				widget.node.appendChild(jupyter_area);
-				//widget_area.appendChild(document.createTextNode("Widget-Bereich"));
-
-
-
-				let widget_code =
-`from ipywidgets import IntSlider, Text, VBox
-from IPython.display import display
-
-s = IntSlider(max=200, value=100)
-t = Text()
-
-def update_text(change=None):
-    t.value = str(s.value ** 2)
-
-s.observe(update_text, names='value')
-update_text()
-display(VBox([s, t]));`
-				//let inputarea = document.getElementsByClassName('inputarea')[0] as HTMLElement;
-				let editor = CodeMirror(input_area, {
-                    			value: widget_code,
-                    			mode: 'python',
-                    			tabSize: 4,
-                    			showCursorWhenSelecting: true,
-                    			viewportMargin: Infinity,
-                    			readOnly: true
-					});
-				console.log(editor);
-				//let widgetarea = document.getElementsByClassName("widgetarea")[0] as HTMLElement;
-					console.log(widget_area);
-					let manager = new WidgetManager(session.kernel, widget_area);
-					console.log(manager);
-
-
-					/*	let widget_request = session.kernel.requestExecute({ code: widget_code });
-                		widget_request.onIOPub = (msg:any) => {
-				// If we have a display message, display the widget.
-					console.log("WIDGET ACTIVATED");
-					console.log(msg);
-					//if(msg.content.type == 'display_data'){
-					//	manager.display_view(msg, 
-					};*/
-
-					let execution = session.kernel.requestExecute({ code: widget_code });
-        execution.onIOPub = (msg:any) => {
-            // If we have a display message, display the widget.
-            if (KernelMessage.isDisplayDataMsg(msg)) {
-                let widgetData: any = msg.content.data['application/vnd.jupyter.widget-view+json'];
-                if (widgetData !== undefined && widgetData.version_major === 2) {
-			console.log("Trying to build model");	
-			let model = manager.get_model(widgetData.model_id);
-			console.log(model);
-			//console.log(model.get('_view_name');
-			//console.log(manager.loadClass(model.get('_view_name'), model.get('_view_module'), model.get('_view_module_version'));
-		    if (model !== undefined) {
-		    	//@ts-ignore
-			model.then(model => {
-				console.log(model.get('_view_name'));
-				console.log("In here");
-				console.log(manager.create_view(model));
-				let view = manager.create_view(model);
-				console.log(view);
-				view.then(view => {
-				//manager.display_view(msg, view, {});
-				//return Promise.resolve(view).then((view) => {Widget.attach(view, this.el);});
-				});
-				//manager.display_model(msg, model);
-				console.log("Afterwards");
-                        });
-                    }
-                }
-            }
-};
 
 				session.kernel.registerCommTarget('test2', (comm:any, commMsg:any):any => {
 					if(commMsg.content.target_name !== 'test2'){
@@ -301,6 +91,11 @@ display(VBox([s, t]));`
 					};
 					comm.onClose = (msg:any) => {};
 				});
+
+
+				// OutputArea
+				
+
 
 				// Initialize function that returns variable list
 				let code: string;
