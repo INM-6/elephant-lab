49b7ba3268725fed7d23880b6abf8780ce17981e
Björn Müller <bj.mueller@fz-juelich.de>
2018-12-20 12:04:54 +0100

    More Code refactoring

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,10 @@ import {
 	IRenderMimeRegistry
 } from '@jupyterlab/rendermime';
 
+import {
+	pythonCode
+} from './kernelcode';
+
 import '../style/index.css';
 
 
@@ -38,136 +42,36 @@ const extension: JupyterLabPlugin<void> = {
 	autoStart: true,
 	requires: [ICommandPalette, INotebookTracker, IRenderMimeRegistry],
 	activate: 
-	(app: JupyterLab, palette: ICommandPalette, consoles: INotebookTracker, rendermime) => {
-			
+	(app: JupyterLab, palette: ICommandPalette, consoles: INotebookTracker, rendermime) => {	
 		console.log('JupyterLab extension neo_elephant is activated!');
-		let widget: Panel = new Panel();
-		widget.id = 'neo_elephant';
-		widget.title.label = 'Visualization';
-		widget.title.closable = true;
-		let var_place = document.createElement('div');
-		var_place.setAttribute("id", "neo_ele_vars");
-		widget.node.appendChild(var_place);
+		
+		// Initialize new Tab in which everything will be displayed
+		// Will be moved to Tab constructor
+		let widget = initializeTab(app);
 
-
-		function ioCallback(msg: KernelMessage.IIOPubMessage): void {
-			console.log("Got return from Kernel");
-			console.log(msg);
-			if(msg.header.msg_type == 'stream' && msg.content.name == 'stdout'){
-				console.log("Stdout: ", msg.content);
-				let text = document.createTextNode(msg.content.text as string);
-				let old_text = var_place.childNodes[0];
-				if(old_text != null){
-					var_place.removeChild(old_text);
-				}
-				var_place.appendChild(text);
-			}
-		}
-
+		// Register event to react on new tab
 		consoles.widgetAdded.connect((sender, consolePanel) => {
 
-			               if (!widget.isAttached) {
-                                        app.shell.addToMainArea(widget);
-                                }
-                                app.shell.activateById(widget.id);
+			// Show tab if it was not yet shown
+			attachTab(widget, app);
 
-			
 			var session: IClientSession = consolePanel.session;
 			console.log("Session.ready: ", session.ready);
 
+			// If session is available in the notebook
+			// And kernel is ready as well
 			session.ready.then(() => {session.kernel.ready.then(() => {
 
-				session.kernel.registerCommTarget('test2', (comm:any, commMsg:any):any => {
-					if(commMsg.content.target_name !== 'test2'){
-						return;
-					}
-					comm.onMsg = (msg:any) => {
-						var c = msg.buffers[0].buffer;
-						c;
-						console.log("Message received");
-						//console.log(c[0]);
-						//console.log("MEEESSSAAAGGEEE ", msg.buffers[0]);
-						//console.log(new Float32Array(msg.buffers[0].buffer));
-					};
-					comm.onClose = (msg:any) => {};
-				});
+				// Register a Comm channel (not needed currently, but might be)
+				registerComm('test2', session);
 
-				let plot_code = 
-`import matplotlib.pyplot as plt
-plt.plot([1,2,3], [4,5,6])
-plt.show()
-`
-				console.log(plot_code);
-				let neo_plot =
-`import matplotlib.pyplot as plt
-plt.plot(range(len(ew_block.segments[0].analogsignals[0])), ew_block.segments[0].analogsignals[0])
-plt.show();
-`
-				// OutputArea
-				let model = new OutputAreaModel({trusted: true});
-				let outarea = new OutputArea({rendermime, model});
-				console.log(outarea);
-				widget.addWidget(outarea);
-				OutputArea.execute(neo_plot, outarea, session);
-				outarea.id = "jup_vis_out_id";
-				//console.log(outarea.id);
+				// Create 2 OutputAreas that will show plots
+				let outarea = createOutput(session, rendermime, widget, ['my-outarea-classs'], 'jup_vis_out_id1', pythonCode['plotCode']);
+				let outarea2 = createOutput(session, rendermime, widget, ['my-outarea-classs'], 'jup_vis_out_id2', pythonCode['neoPlot']);
 
-                 // OutputArea
-       let model2 = new OutputAreaModel({trusted: true});
-       let outarea2 = new OutputArea({rendermime, model: model2});
-        console.log(outarea2);
-        widget.addWidget(outarea2);
-        OutputArea.execute(plot_code, outarea2, session);
-			outarea2.id = "jup_vis_out_id2"
-
-				// Initialize function that returns variable list
-				let code: string;
-				/*fetch('packages/python/test.py').then(response => {
-					console.log("Reading code: ", response)
-					return response.json();
-				}).then(data => {
-					code = data;
-					});*/
-
-				code = 
-`import json
-from IPython.core.magics.namespace import NamespaceMagics
-from IPython import get_ipython
-#from neo.core import BaseNeo
-nsm = NamespaceMagics()
-nsm.shell = get_ipython().kernel.shell
-
-def testfunc():
-    nsm = NamespaceMagics()
-    nsm.shell = get_ipython().kernel.shell
-    from neo.core.baseneo import BaseNeo
-    from neo.core.block import Block
-    vals = nsm.who_ls()
-    values = [v for v in vals if isinstance(eval(v), (BaseNeo, list))] # in ['Block', 'Segment', 'ChannelIndex', 
-    for value in list(values):
-        val = eval(value)
-        if isinstance(val, list):
-            if len(val) > 0 and isinstance(val[0], BaseNeo):
-                    pass
-            else:
-                values.remove(value)
-        if isinstance(val, Block):
-            for i, seg in enumerate(val.list_children_by_class("Segment")):
-                #globals()[''.join(['blchidx', str(i)])] = chidx
-                values.append(''.join([value, '.segmets[', str(i), ']']))
-                for j, sig in enumerate(seg.analogsignals):
-                    #pass
-                    values.append(''.join([values[-1-j], '.analogsigs[', str(j), ']']))
-
-    return json.dumps(values)`
-				
-				console.log('Python Code: ', code);
-				let request_init: KernelMessage.IExecuteRequest = {
-					code: code,
-					stop_on_error: false,
-					store_history: false,
-				};
-				session.kernel.requestExecute(request_init);
+	
+				// Execute code in kernel
+				executeCode(pythonCode['testfunc'], session);
 
 				// React to codecell execution, update variable list
 				NotebookActions.executed.connect(() => {
@@ -175,19 +79,11 @@ def testfunc():
 					console.log("Cell executed"); 
 
 					// Plot analogsignal (test)
-					OutputArea.execute(neo_plot, outarea, session);
+					OutputArea.execute(pythonCode['neoPlot'], outarea, session);
+					OutputArea.execute(pythonCode['plotCode'], outarea2, session);
 
-					let request: KernelMessage.IExecuteRequest = {
-						code: "print(testfunc())",
-						stop_on_error: false,
-						store_history: false,
-					};
-
-					let future: Kernel.IFuture = session.kernel.requestExecute(request);
-
-					future.onIOPub = ( ( msg: KernelMessage.IIOPubMessage ) =>
-						{ ioCallback( msg );});
-
+					// Update list of variables
+					executeCode('print(testfunc())', session, ioCallback);
 				});
 
 			});});
@@ -195,32 +91,127 @@ def testfunc():
 			console.log("Connected to currently active Notebook");
 		
 		});
+		
+		// Add some random text, nothing useful here
+		addTextToPanel(widget, 'This is a text');
 
-		// Adding content to my tab
-		let new_content = document.createElement('div');
-		let text = document.createTextNode("This a text.");
-		new_content.appendChild(text);
-		widget.node.appendChild(new_content);
-
-		const command: string = 'neo:open';
-
-		app.commands.addCommand(command, {
-			label: 'Visualize neo and elephant',
-			execute: () => {
-				if (!widget.isAttached) {
-					app.shell.addToMainArea(widget);
-				}
-				app.shell.activateById(widget.id);
-			}
-		});
-
-		palette.addItem({command, category: 'Tutorial'});
+		// Place command into CommandPalette
+		// This command will open the tab			let widget = initializeTab(app);
+		createCommand('neo:open', widget, app, palette);
 
 	}
 };
 
-//function add_text(text: string, parent_elem: any){
-	//let new_content = document.createElement('div');
-//}
+// Adds an OutputArea to the tab 'widget'
+function createOutput(session: IClientSession, rendermime: IRenderMimeRegistry, widget: Panel, cls: string[], id: string, code: string): OutputArea{
+	// OutputArea
+	let model = new OutputAreaModel({trusted: true});
+	let outarea = new OutputArea({rendermime, model});
+	console.log(outarea);
+	widget.addWidget(outarea);
+	OutputArea.execute(code, outarea, session);
+	outarea.id = id;
+	for(let currCls of cls){
+		outarea.addClass(currCls);
+	}
+	return outarea;
+}
+
+function addTextToPanel(widget: Panel, text: string){
+	// Adding content to my tab
+	let new_content = document.createElement('div');
+	let textField = document.createTextNode(text);
+	new_content.appendChild(textField);
+	widget.node.appendChild(new_content);
+}
+
+function initializeTab(app: JupyterLab){
+	let widget: Panel = new Panel();
+	widget.id = 'neo_elephant';
+	widget.title.label = 'Visualization';
+	widget.title.closable = true;
+	let var_place = document.createElement('div');
+	var_place.setAttribute("id", "neo_ele_vars");
+	widget.node.appendChild(var_place);
+	return widget;
+}
+
+function attachTab(widget: Panel, app: JupyterLab){
+	// Attach tab if not yet attached
+	if (!widget.isAttached) {
+		app.shell.addToMainArea(widget);
+	}
+	app.shell.activateById(widget.id);
+}
+
+function createCommand(command: string, widget: Panel, app: JupyterLab, palette: ICommandPalette){
+	app.commands.addCommand(command, {
+		label: 'Visualize neo and elephant',
+		execute: () => {
+			attachTab(widget, app)
+		}
+	});
+	palette.addItem({command, category: 'Tutorial'});
+}
+
+function ioCallback(msg: KernelMessage.IIOPubMessage): void {
+	console.log("Got return from Kernel");
+	console.log(msg);
+	if(msg.header.msg_type == 'stream' && msg.content.name == 'stdout'){
+		console.log("Stdout: ", msg.content);
+		let text = document.createTextNode(msg.content.text as string);
+		let var_place = document.getElementById('neo_ele_vars');
+		let old_text = var_place.childNodes[0];
+		if(old_text != null){
+			var_place.removeChild(old_text);
+		}
+		var_place.appendChild(text);
+	}
+}
+
+function registerComm(name: string, session: IClientSession){
+	session.kernel.registerCommTarget('test2', (comm:any, commMsg:any):any => {
+		if(commMsg.content.target_name !== 'test2'){
+			return;
+		}
+		comm.onMsg = (msg:any) => {
+			var c = msg.buffers[0].buffer;
+			c;
+			console.log("Message received");
+			//console.log(c[0]);
+			//console.log("MEEESSSAAAGGEEE ", msg.buffers[0]);
+			//console.log(new Float32Array(msg.buffers[0].buffer));
+		};
+		comm.onClose = (msg:any) => {};
+	});
+}
+
+function executeCode(code: string, session: IClientSession, callback?: any){
+	let request: KernelMessage.IExecuteRequest = {
+		code: code,
+		stop_on_error: false,
+		store_history: false,
+	};
+
+	let future: Kernel.IFuture = session.kernel.requestExecute(request);
+
+	if(callback){
+		future.onIOPub = ( ( msg: KernelMessage.IIOPubMessage ) => {
+			callback( msg );
+		});
+	}
+}
+
+// This is code that is not yet in use
+// But can be used as template in case anything needs to be fetched
+// @ts-ignore
+function fetchText(){
+	fetch('packages/python/test.py').then(response => {
+		console.log("Reading code: ", response);
+		return response.json();
+	}).then(data => {
+		return data;
+	});
+}
 
 export default extension;
