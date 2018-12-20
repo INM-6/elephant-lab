2f34490d99b0b67e0265976d1f3ab50f204a5ce9
Björn Müller <bj.mueller@fz-juelich.de>
2018-12-20 11:44:06 +0100

    Refactored the code
    Moved a lot of stuff to functions
    
    Next step: Introduce OOP

---
+++ src/kernelcode.ts
@@ -0,0 +1,49 @@
+let code = 
+`import json
+from IPython.core.magics.namespace import NamespaceMagics
+from IPython import get_ipython
+#from neo.core import BaseNeo
+nsm = NamespaceMagics()
+nsm.shell = get_ipython().kernel.shell
+
+def testfunc():
+    nsm = NamespaceMagics()
+    nsm.shell = get_ipython().kernel.shell
+    from neo.core.baseneo import BaseNeo
+    from neo.core.block import Block
+    vals = nsm.who_ls()
+    values = [v for v in vals if isinstance(eval(v), (BaseNeo, list))] # in ['Block', 'Segment', 'ChannelIndex', 
+    for value in list(values):
+        val = eval(value)
+        if isinstance(val, list):
+            if len(val) > 0 and isinstance(val[0], BaseNeo):
+                    pass
+            else:
+                values.remove(value)
+        if isinstance(val, Block):
+            for i, seg in enumerate(val.list_children_by_class("Segment")):
+                #globals()[''.join(['blchidx', str(i)])] = chidx
+                values.append(''.join([value, '.segmets[', str(i), ']']))
+                for j, sig in enumerate(seg.analogsignals):
+                    #pass
+                    values.append(''.join([values[-1-j], '.analogsigs[', str(j), ']']))
+
+    return json.dumps(values)`;
+
+let neo_plot =
+`import matplotlib.pyplot as plt
+plt.plot(range(len(ew_block.segments[0].analogsignals[0])), ew_block.segments[0].analogsignals[0])
+plt.show();
+`;
+
+let plot_code = 
+`import matplotlib.pyplot as plt
+plt.plot([1,2,3], [4,5,6])
+plt.show()
+`;
+
+export const pythonCode = {
+	'neoPlot': neo_plot,
+	'plotCode': plot_code,
+	'testfunc': code
+};
