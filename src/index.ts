5d929f936d4713cafd10f6eba8bffb2d76645f7d
Maximilian Kramer <m.kramer@fz-juelich.de>
2022-10-25 16:00:19 +0200

    added imports according to INM-6/jupyphant, replaced @phosphor with @lumino because this library was renamed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,55 @@
+// Imports for Jupyter
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILayoutRestorer
 } from '@jupyterlab/application';
+import {
+	ICommandPalette,
+	IClientSession,
+	WidgetTracker
+} from '@jupyterlab/apputils';
+import {
+	INotebookTracker,
+	NotebookActions,
+	NotebookPanel
+} from '@jupyterlab/notebook';
+import {
+	KernelMessage
+} from '@jupyterlab/services';
+
+// Note: SimplifiedOutputArea seems to simply behave like
+// the regular OutputAreas inside the notebook
+// Currently trying to use regular OutputAreas
+// because they *might* have more features
+// In caseof problems, use Simplified
+import {
+	OutputArea,
+	OutputAreaModel
+} from '@jupyterlab/outputarea';
+
+import {
+	IRenderMimeRegistry
+} from '@jupyterlab/rendermime';
+
+// Lumino imports for dealing with the tabs within JupyterLab
+// These are called Panels
+import {
+	 SplitPanel, Panel
+} from '@lumino/widgets';
+//@ts-ignore: TODO: Why is this necessary?
+import {
+  JSONExt
+} from '@lumino/coreutils';
+
+// Own imports
+// Python Code to execute in the kernel
+import {
+	pythonCode
+} from './kernelcode';
+// Style from css
+import '../style/index.css';
+
 
 /**
  * Initialization data for the jupyphant extension.
