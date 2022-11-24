1510cf252d0ca65d8f1fec9a6697202488818186
Maximilian Kramer <m.kramer@fz-juelich.de>
2022-11-24 11:45:52 +0100

    added call-back function which will be executed when a node is selected

--- src/kernelcode.ts
+++ src/kernelcode.ts
@@ -36,9 +36,37 @@ let lfp_plot =
 `my_jupyphant_vis_xxx.plot_anasig();
 `;
 // Call to the function that initializes the ipytree widget with an empty tree
-let create_tree = 
-`from IPython.display import display
-display(my_jupyphant_vis_xxx.create_tree())
+let create_tree =
+`import matplotlib.pyplot as plt
+import IPython
+from IPython.display import display
+from ipywidgets import link, HBox, VBox, IntSlider, Text, Label, Output
+
+my_jupyphant_vis_xxx.create_tree()
+my_jupyphant_vis_xxx.tree.layout.width = '50%'
+
+output_node_info = Output()
+output_node_plot = Output()
+
+def on_selected_change(change):
+    with output_node_info:
+        IPython.display.clear_output()
+#        print('Some node selected!')
+        for i in range(len(change["new"])):
+            print(f'{i}. {change["new"][i].name} -> urn_id = {change["new"][i]._id}')
+    with output_node_plot:
+        IPython.display.clear_output()
+        selected_ids = [my_jupyphant_vis_xxx.map[node._id] for node in my_jupyphant_vis_xxx.tree.selected_nodes if my_jupyphant_vis_xxx.map[node._id] is not None]
+        print(f'Python Ids of selected nodes {selected_ids}')
+        my_jupyphant_vis_xxx.plot_sptr(selected_ids=selected_ids)
+        plt.show()
+        #print('after plot')
+
+my_jupyphant_vis_xxx.tree.observe(on_selected_change, names='selected_nodes')
+
+right_side = VBox([Label("Selected Node:"), output_node_info, output_node_plot])
+right_side.layout.width = '50%'
+HBox([my_jupyphant_vis_xxx.tree, right_side])
 `;
 // Call to the function that updates the ipytree tree view of the neo hierarchy
 let update_tree =
