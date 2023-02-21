03628adaa8ac3bdd4d569c93d518dbbd285206fe
Maximilian Kramer <m.kramer@fz-juelich.de>
2023-02-21 16:27:40 +0100

    display now each pandas.dataframe of an neo-object per top node

--- src/kernelcode.ts
+++ src/kernelcode.ts
@@ -94,14 +94,18 @@ def on_selected_change(change):
         # for i in range(len(change["new"])):
         #    print(f'{i}. {change["new"][i].name} -> urn_id = {change["new"][i]._id}')
         df_anasig, df_spt, df_evt, df_epc = my_jupyphant_vis_xxx.selected_nodes_to_dataframes(selected_ids=selected_ids)
-        if df_anasig is not None:
-            display(df_anasig)
-        if df_spt is not None:
-            display(df_spt)
-        if df_evt is not None:
-            display(df_evt)
-        if df_epc is not None:
-            display(df_epc)
+        if df_anasig:
+            for df in df_anasig:
+                display(df)
+        if df_spt:
+	        for df in df_spt:
+	            display(df)
+        if df_epc:
+            for df in df_epc:
+                display(df)
+        if df_evt:
+	        for df in df_evt:
+	            display(df)
     with output_node_plot:
         IPython.display.clear_output()
         # print(f'Python Ids of selected nodes {selected_ids}')
