67997d926ea14a0bcc99d614ea17e1422e7a3580
tm4185s <tobias.michels@alumni.fh-aachen.de>
2026-02-09 15:11:07 +0100

    [FIX] Added attributes to hash function
    This resolves an issue where changing an attribute of a neo object would not be registered by Jupyphant because the hash stayed the same

--- jupyphant/jupyphant.py
+++ jupyphant/jupyphant.py
@@ -249,7 +249,10 @@ class Jupyphant:
                 str(neo_obj.units),
                 float(neo_obj.sampling_rate),
                 str(neo_obj.sampling_rate),
-                float(neo_obj.t_start)
+                float(neo_obj.t_start),
+                neo_obj.name,
+                neo_obj.description,
+                neo_obj.annotations
             )
             result = joblib.hash(hashable_summary, hash_name=hash_name)
 
@@ -257,7 +260,10 @@ class Jupyphant:
             hashable_summary = (
                 neo_obj.magnitude,
                 str(neo_obj.units),
-                float(neo_obj.t_start)
+                float(neo_obj.t_start),
+                neo_obj.name,
+                neo_obj.description,
+                neo_obj.annotations
             )
             result = joblib.hash(hashable_summary, hash_name=hash_name)
 
@@ -266,7 +272,10 @@ class Jupyphant:
                 neo_obj.times,
                 str(neo_obj.units),
                 float(neo_obj.t_start),
-                float(neo_obj.t_stop)
+                float(neo_obj.t_stop),
+                neo_obj.name,
+                neo_obj.description,
+                neo_obj.annotations
             )
             result = joblib.hash(hashable_summary, hash_name=hash_name)
 
@@ -274,7 +283,10 @@ class Jupyphant:
             hashable_summary = (
                 neo_obj.times,
                 neo_obj.labels,
-                str(neo_obj.units)
+                str(neo_obj.units),
+                neo_obj.name,
+                neo_obj.description,
+                neo_obj.annotations
             )
             result = joblib.hash(hashable_summary, hash_name=hash_name)
 
