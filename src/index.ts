f69477822cf9e14f3ec745e0789ed42b4b3ffe02
Jan Nolten <j.nolten@fz-juelich.de>
2026-02-23 16:28:13 +0100

    [Remove] removed Jans Version of removing the spacing on the left of the widget

--- src/index.ts
+++ src/index.ts
@@ -855,7 +855,7 @@ except Exception as e:
 		let explorer_widget_raw_plot = new Panel();
 		explorer_widget_raw_plot.title.label = 'Explore';
 		explorer_widget_raw_plot.node.style.cssText = explorer_widget_raw_plot.node.style.cssText + ' overflow-x: scroll; overflow-y: scroll;';
-		this.outarea_nodeexplorer_raw = this.createOutputArea(rendermime, explorer_widget_raw_plot, ['my-outarea-class', 'no-left-spacing'], 'jup_vis_out_id_2.2', session);
+		this.outarea_nodeexplorer_raw = this.createOutputArea(rendermime, explorer_widget_raw_plot, ['my-outarea-class'], 'jup_vis_out_id_2.2', session);
 
 		this.widget.addWidget(tree_widget);
 		this.widget.addWidget(explorer_widget_info, { mode: 'split-bottom', ref: tree_widget });
